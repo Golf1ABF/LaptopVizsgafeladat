38d79206d94110eb04dbf2f2b9031cf4124a329f
Avx <93741404+Golf1ABF@users.noreply.github.com>
2025-02-13 11:21:50 +0100

    Add project files.

---
+++ LaptopCli/Laptop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopCli
+{
+    internal class Laptop
+    {
+        public Category Category{ get; set; }
+        public string CPU { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string OS { get; set; }
+        public double Price { get; set; }
+        public string Ram { get; set; }
+        public string Screen { get; set; }
+        public string Storage { get; set; }
+
+        public Laptop(string sor)
+        {
+            string[] s = sor.Split(";");
+            this.Category = new Category(int.Parse(s[0]), s[1]);
+            this.CPU = s[2];
+            this.Manufacturer = new Manufacturer(int.Parse(s[3]), s[4]);
+            this.Model = s[5];
+            this.OS = s[6];
+            this.Price = double.Parse(s[7]);
+            this.Ram = s[8];
+            this.Screen = s[9];
+            this.Storage = s[10];
+        }
+
+        public override string ToString()
+        {
+            return $"{Category.CategoryName} | {Model} ({CPU}) | {OS}";
+        }
+
+    }
+}
---
+++ LaptopCli/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace LaptopCli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Laptop> list = new List<Laptop>();
+            var sr = new StreamReader("../../../src/laptops.txt");
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                list.Add(new Laptop(sr.ReadLine()));
+            }
+
+            Console.WriteLine("5.Feladat");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString() + $"| {Math.Round(item.Price * 4.12, 0)} HUF");
+            }
+
+            Console.WriteLine("6. Feladat");
+            var intelProcSSD = list
+                .Where(x => x.CPU.ToLower().Contains("i7") && x.Storage.ToLower().Contains("ssd"))
+                .ToList();
+            for (int i = 0; i < intelProcSSD.Count; i++)
+            {
+                Console.WriteLine($"[{i+1}] {intelProcSSD[i]}");
+            }
+
+            var avgPriceForPrevious = intelProcSSD.Average(x => x.Price);
+            Console.WriteLine($"Átlag áruk: {avgPriceForPrevious} INR");
+
+            Console.WriteLine("7. Feladat cheap.txt fájlba írva");
+
+            var twentyCheapest = list
+                .Where(x => x.Category.CategoryName == "Gaming")
+                .OrderBy(x => x.Price)
+                .Take(20)
+                .ToList();
+
+            var sw = new StreamWriter("../../../src/cheap.txt");
+            foreach (var item in twentyCheapest)
+            {
+                sw.WriteLine($"{item.Manufacturer.ManufacturerName}, {item.Model} " +
+                    $"\n {item.CPU} " +
+                    $"\n {item.Storage} " +
+                    $"\n {item.Screen}");
+            }
+            sw.Close();
+        }
+    }
+}
